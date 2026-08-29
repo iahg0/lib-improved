5fe1e4019d67eae65107ba7c94083a20db1b2d46
iahg <3072835264@qq.com>
2026-08-29 23:22:59 +0800

    Change update/download URL to GitHub raw

--- FC2CMADB-improved.js
+++ FC2CMADB-improved.js
@@ -9,8 +9,8 @@
 // @grant        GM_addStyle
 // @grant        GM_xmlhttpRequest
 // @license      MIT
-// @downloadURL [https://update.greasyfork.org/scripts/583333/FC2CMADB-improved.user.js](https://update.greasyfork.org/scripts/583333/FC2CMADB-improved.user.js)
-// @updateURL [https://update.greasyfork.org/scripts/583333/FC2CMADB-improved.meta.js](https://update.greasyfork.org/scripts/583333/FC2CMADB-improved.meta.js)
+// @downloadURL https://raw.githubusercontent.com/iahg0/FC2CMADB-improved/master/FC2CMADB-improved.js
+// @updateURL https://raw.githubusercontent.com/iahg0/FC2CMADB-improved/master/FC2CMADB-improved.js
 // ==/UserScript==
 (function() {
 'use strict';
